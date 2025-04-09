6edf841d6f35d0bc79f423cc3ac7e55170fc1be5
Germo Eismann <germo@eba.ee>
2025-04-09 13:55:45 +0300

    Parandasin EmailJS SDK veateate ja uuendasin skripti

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';  // Õige import Vue 3 puhul
 import App from './App.vue';  // Importige App.vue fail
+import emailjs from 'emailjs-com';
+
+emailjs.init("ljSj1so40G933f40z"); // <-- Asenda see päris user ID-ga
 
 createApp(App).mount('#app');  // Rakenduse loomine ja mountimine
