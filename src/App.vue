5ea9b16c4069b20be13ea7717cb107310a639bb1
Germo Eismann <germo@eba.ee>
2025-04-09 12:34:03 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Vue rakendus töötab!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Siin saad lisada oma rakenduse stiile */
+</style>
