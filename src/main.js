d99f9cf713e0b20072739388483b9600741dc2b9
Germo Eismann <germo@eba.ee>
2025-04-09 12:34:03 +0300

    First commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';  // Õige import Vue 3 puhul
+import App from './App.vue';  // Importige App.vue fail
+
+createApp(App).mount('#app');  // Rakenduse loomine ja mountimine
